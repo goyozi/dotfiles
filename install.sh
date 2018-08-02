fc17e44536a387fca740c454559ef5c5601bf1cf
Grzegorz Ziemonski <grzegorz.ziemonski@zooplus.com>
2018-08-02 15:52:46 +0200

    Automate environment configuration

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #! /usr/bin/env bash
 
-sources=$(pwd)
-
-ln -sf $sources/i3 ~/.config/i3
-ln -sf $sources/urxvt ~/.config/urxvt
-ln -sf $sources/polybar ~/.config/polybar
-
-echo -e "#!/bin/sh\nfeh --bg-fill $sources/wallpaper.jpg" > ~/.fehbg
-chmod +x ~/.fehbg
-
+git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
+ansible-playbook -i hosts playbook.yml
